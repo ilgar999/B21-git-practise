3290edea9b6929aee9c20bfff6a7cd88b7c161ec
IlgarA <ilqar.nureddinoglu@gmail.com>
2021-01-26 15:38:38 -0500

    movie 2 branch 20min

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -5,5 +5,6 @@ public class Hello {
         System.out.println("Hello World, B21");
         System.out.println("B21 feature SDET learning Git");
         // movie on the phone
+        // movie on the phone sdvd
     }
 }
