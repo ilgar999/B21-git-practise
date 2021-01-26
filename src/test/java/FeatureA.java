8883385fa63d0f5766abae35a2b649eb4193b6c2
IlgarA <ilqar.nureddinoglu@gmail.com>
2021-01-26 14:23:44 -0500

    Started working

--- src/test/java/FeatureA.java
+++ src/test/java/FeatureA.java
@@ -2,6 +2,7 @@ public class FeatureA {
 
     public static void main(String[] args) {
         System.out.println("Awesome work for A started");
+        System.out.println("B21 learning on new Feature A");
     }
 
 }
